b5c5852e3c44ca6d7024d967dd71b3e1689b6cc2
kschoi-wev <kschoi@weversecompany.com>
2023-12-28 14:05:12 +0900

    refactor: remote component factory

--- shellts/src/components/remote.tsx
+++ shellts/src/components/remote.tsx
@@ -3,54 +3,68 @@ import PokemonList, { PokemonListProps } from "@mf-types/apollots/PokemonList";
 import Content, { ContentProps } from "@mf-types/nested1ts/Content";
 import RemoteRoute from "@mf-types/routets/Route";
 import ErrorBoundary from "./common/error-boundary";
+import { timeout } from "promise-timeout";
 
-export function Loading() {
+const REMOTE_COMPONENTS_FACTORY = {
+  "nested1ts/Content": () => {
+    if (typeof window !== "undefined")
+      return lazy(() =>
+        // @ts-expect-error federated module.
+        timeout(import("nested1ts/Content"), 2000)
+      );
+  },
+  "apollots/PokemonList": () => {
+    if (typeof window !== "undefined")
+      return lazy(() =>
+        // @ts-expect-error federated module.
+        timeout(import("apollots/PokemonList"), 2000)
+      );
+  },
+  "routets/Route": () => {
+    if (typeof window !== "undefined")
+      return lazy(() =>
+        // @ts-expect-error federated module.
+        timeout(import("routets/Route"), 2000)
+      );
+  },
+};
+
+function Loading() {
   return <div>loading...</div>;
 }
 
+export function useGetComponent<T>(
+  name: keyof typeof REMOTE_COMPONENTS_FACTORY
+) {
+  const [Component, setComponent] = useState<T>();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setComponent(REMOTE_COMPONENTS_FACTORY[name]() as T);
+    }
+  }, []);
+
+  if (!Component) return Loading;
+  return Component;
+}
+
 export function RContent(props: ContentProps) {
-  const [Component, setComponent] = useState<typeof Content>();
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setComponent(
-        // @ts-expect-error federated module.
-        lazy(() => import("nested1ts/Content")) as unknown as typeof Content
-      );
-    }
-  }, []);
-  if (!Component) return <Loading />;
+  const Component = useGetComponent<typeof Content>("nested1ts/Content");
   return (
     <ErrorBoundary>
       <Component {...props} />
     </ErrorBoundary>
   );
 }
-
 export function RPokemonList(props: PokemonListProps) {
-  const [Component, setComponent] = useState<typeof PokemonList>();
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // @ts-expect-error federated module.
-      setComponent(lazy(() => import("apollots/PokemonList")));
-    }
-  }, []);
-  if (!Component) return <Loading />;
+  const Component = useGetComponent<typeof PokemonList>("apollots/PokemonList");
   return (
     <ErrorBoundary>
       <Component {...props} />
     </ErrorBoundary>
   );
 }
-
 export function RRoute() {
-  const [Component, setComponent] = useState<typeof RemoteRoute>();
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // @ts-expect-error federated module.
-      setComponent(lazy(() => import("routets/Route")));
-    }
-  }, []);
-  if (!Component) return <Loading />;
+  const Component = useGetComponent<typeof RemoteRoute>("routets/Route");
   return (
     <ErrorBoundary>
       <Component />
